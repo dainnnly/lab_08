8bf0e8be7003655b59bae042c75f83c4e22237b2
Samsung <Samsung@Samsung-PC>
2016-05-20 11:08:40 +0900

    git testing..

--- src/lab_08/lab_08.java
+++ src/lab_08/lab_08.java
@@ -1,6 +1,18 @@
 package lab_08;
 
 public class lab_08 {
-	
+	private String units;
+	private long amount;
+	private int cents;
+
+	public lab_08(double amount, String code) {
+		this.units = code;
+		setAmount(amount);
+	}
+
+	private void setAmount(double amount) {
+		this.amount = new Double(amount).longValue();
+		this.cents = (int) ((amount * 100.0) % 100);
+	}
 
 }
